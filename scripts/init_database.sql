0350b37b4f69b33a7cd0f8a72d460d8dbb2dbf01
Jenil133 <72555436+Jenil133@users.noreply.github.com>
2025-07-11 15:11:25 -0700

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+
+/* 
+
+============================
+Create Database Schema 
+============================
+These scripts sets up three schema withing the database : bronze, silver and gold.
+
+*/
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
